331d505db25f35a08b94f903c4c49b233d06e7b6
oneday <2278869297@qq.com>
2026-09-09 11:34:43 +0800

    feat: P0 save/load loop (SaveService, JsonSaveService, GameManager, scene assembly, gitignore)

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,6 +1,7 @@
 module com.fieldstory.farm {
     requires javafx.controls;
     requires javafx.fxml;
+    requires com.fasterxml.jackson.databind;
 
     opens com.fieldstory.farm to javafx.fxml;
     exports com.fieldstory.farm;
