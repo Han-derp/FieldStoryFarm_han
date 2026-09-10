9cb3ff89317d412ce9694124064b69853c2bcf43
HanSiyuan <1936293136@qq.com>
2026-09-10 15:09:24 +0800

    feat(p0-economy): complete economy constants and validation

--- src/test/java/com/fieldstory/farm/service/economy/EconomyServiceImplTest.java
+++ src/test/java/com/fieldstory/farm/service/economy/EconomyServiceImplTest.java
@@ -1,10 +1,11 @@
-package com.fieldstory.farm.service.economy.impl;
+package com.fieldstory.farm.service.economy;
 
 import com.fieldstory.farm.model.CropType;
 import com.fieldstory.farm.model.Player;
 import com.fieldstory.farm.model.economy.PurchaseResult;
 import com.fieldstory.farm.service.economy.EconomyService;
 
+import com.fieldstory.farm.service.economy.impl.EconomyServiceImpl;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
